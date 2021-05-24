d8715cad661e6ad18b532153796227bfd5b29cca
Polina Plesovskih <78843788+tsv-mindal@users.noreply.github.com>
2021-05-24 21:15:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,73 +1,67 @@
 // Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+#include <cassert>
 template<typename T>
-class TPQueue  {
-  private:
+class TPQueue {
+ private:
   T* arr;
   int size;
   int begin, end;
   int count;
   int stepBack(int index) {
-    int r = --index;
-    if (r < 0)
-      r += size + 1;
-    return r;
+  int res = --index;
+  if (res < 0)
+  res += size + 1;
+  return res;
   }
   int stepForward(int index) {
-    int r = ++index;
-    if (r > size)
-      r -= size + 1;
-    return r;
+  int res = ++index;
+  if (res > size)
+  res -= size + 1;
+  return res;
   }
-  public:
-  TPQueue( ):
+
+ public:
+  TPQueue() :
   size(100),
   begin(0), end(0), count(0) {
-    arr = new T[size + 1];
+  arr = new T[size + 1];
   }
   ~TPQueue() {
-    delete[] arr;
+  delete[] arr;
   }
-  void push(const T& item) {
-    assert(count < size);
-    if (isEmpty()) {
-      arr[end] = item;
-    } else {
-      int current = stepBack(end);
-      if (arr[current].prior >= item.prior) {
-        arr[end] = item;
-      } else {
-        do {
-          arr[stepForward(current)] = arr[current];
-          current = stepBack(current);
-        } while (arr[current].prior < item.prior &&
-                 stepForward(current) != begin);
-        arr[stepForward(current)] = item;
-      }
-    }
-    end = stepForward(end);
-    count++;
-  }
-  T pop() {
-    assert(count > 0);
-    T item = arr[begin];
-    count--;
-    begin = stepForward(begin);
-    return item;
-  }
-  T get() const {
-    assert(count > 0);
-    return arr[begin];
-  } 
-  bool isEmpty() const {
-    return count == 0;
-  }
-  bool isFull() const {
-    return count == size;
-  }
+void push(const T & item) {
+assert(count < size);
+int cur = end;
+while (begin != cur && item.prior > arr[stepBack(cur)].prior) {
+arr[cur] = arr[stepBack(cur)];
+cur = stepBack(cur);
+}
+arr[cur] = item;
+end = stepForward(end);
+count++;
+}
+T pop() {
+assert(count > 0);
+T item = arr[begin];
+count--;
+begin = stepForward(begin);
+return item;
+}
+T get() const {
+assert(count > 0);
+return arr[begin];
+}
+bool isEmpty() const {
+return count == 0;
+}
+bool isFull() const {
+return count == size;
+}
 };
 struct SYM {
-  char ch;
-  int  prior;
+char ch;
+int  prior;
 };
-
 #endif // INCLUDE_TPQUEUE_H_
